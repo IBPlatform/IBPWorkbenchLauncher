14b17baf1cdeb53794d2f1550387769c254d5408
glenn <glenn@efficio.us.com>
2012-06-11 10:11:44 +0000

    added demo scripts

---
+++ demo_scripts/01_ibdb_cowpea_local-1-Start.bat
@@ -0,0 +1,6 @@
+taskkill /T /F /IM javaw.exe
+..\mysql\bin\mysqladmin.exe --defaults-file=..\mysql\my.ini -u root shutdown
+rmdir /S /Q ..\mysql\data\ibdb_cowpea_local
+mkdir ..\mysql\data\ibdb_cowpea_local
+copy "dump\ibdb_cowpea_local-1-Start" ..\mysql\data\ibdb_cowpea_local
+pause
