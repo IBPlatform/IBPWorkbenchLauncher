0e2ecb759f6805a7e7920a7104b757d87d35e6e6
glenn <glenn@efficio.us.com>
2012-08-13 09:31:29 +0000

    added support for stopping workbench when installer is run added support for running workbench after finishing installation

--- src/main/java/org/generationcp/ibpworkbench/install4j/action/StopWorkbenchAction.java
+++ src/main/java/org/generationcp/ibpworkbench/install4j/action/StopWorkbenchAction.java
@@ -2,15 +2,26 @@ package org.generationcp.ibpworkbench.install4j.action;
 
 import java.io.IOException;
 
-import com.install4j.api.Util;
-import com.install4j.api.actions.AbstractInstallAction;
+import com.install4j.api.actions.AbstractInstallOrUninstallAction;
+import com.install4j.api.actions.InstallAction;
+import com.install4j.api.actions.UninstallAction;
+import com.install4j.api.context.Context;
 import com.install4j.api.context.InstallerContext;
+import com.install4j.api.context.UninstallerContext;
 import com.install4j.api.context.UserCanceledException;
 
-public class StopWorkbenchAction extends AbstractInstallAction {
+public class StopWorkbenchAction extends AbstractInstallOrUninstallAction implements InstallAction, UninstallAction{
     private static final long serialVersionUID = 1L;
 
     public boolean install(InstallerContext context) throws UserCanceledException {
+        return stopWorkbench(context);
+    }
+
+    public boolean uninstall(UninstallerContext context) throws UserCanceledException {
+        return stopWorkbench(context);
+    }
+    
+    protected boolean stopWorkbench(Context context) {
         // taskkill /T /F /IM ibpworkbench.exe
         ProcessBuilder pb = new ProcessBuilder("taskkill", "/T", "/F", "/IM", "ibpworkbench.exe");
         pb.directory(context.getInstallationDirectory());
@@ -19,8 +30,6 @@ public class StopWorkbenchAction extends AbstractInstallAction {
             pb.start();
         }
         catch (IOException e) {
-            Util.showErrorMessage(context.getMessage("cannot_stop_workbench"));
-            return false;
         }
         
         return true;
